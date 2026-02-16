2a541d29ebd97727a4240c58e44d5dcfd154156a
Alex Frolov <fr.280907@gmail.com>
2026-02-16 18:16:42 +0100

    finish

--- js/burger.js
+++ js/burger.js
@@ -5,7 +5,26 @@ document.addEventListener("DOMContentLoaded", () => {
     burger = document.querySelector(".burger"),
     info = document.querySelector(".burgerInfo");
 
+  const item = document.querySelectorAll(".burgerNavItem");
+  const btn = document.querySelector(".burgerCtaBtn");
+
   burgerBtn.addEventListener("click", () => {
+    burger.classList.toggle("is-active");
+    info.classList.toggle("slide-bottom");
+    burgerBtn.classList.toggle("closeBtn");
+    document.body.classList.toggle("no-scroll");
+  });
+
+  item.forEach((item) => {
+    item.addEventListener("click", () => {
+      burger.classList.toggle("is-active");
+      info.classList.toggle("slide-bottom");
+      burgerBtn.classList.toggle("closeBtn");
+      document.body.classList.toggle("no-scroll");
+    });
+  });
+
+  btn.addEventListener("click", () => {
     burger.classList.toggle("is-active");
     info.classList.toggle("slide-bottom");
     burgerBtn.classList.toggle("closeBtn");
